9741c0892f9acc915379dfbd43e3b435ed72f3c3
cburtoncareers <63371868+cburtoncareers@users.noreply.github.com>
2020-10-01 23:40:31 -0400

    added submit button

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Welcome from "./components/welcome/Welcome";
 import Clock from "./components/clock/Clock";
 import Contact from "./components/contact/Contact";
 import Error from "./components/error/Error"
+import Navigation from "./components/navigation/Navigation"
 //Import the Route component
+import Jeopardy from "./components/jeopardy/Jeopardy"
 
-import Navigation from "./components/navigation/Navigation";
 import { Route, Switch } from "react-router-dom";
+
 
 
 class App extends Component {
@@ -27,6 +29,7 @@ class App extends Component {
         <Route path="/welcome/:name" render={(props) => <Welcome{...props} name={props.match.params.name} />} />
         <Route path="/clock" component={Clock} />
         <Route path="/contact" component={Contact} />
+        <Route path="/jeopardy" component={Jeopardy} />
         <Route component ={Error}/>
         </Switch>
       </div>
